f9a06dbbd263eae528152a39e8ea34a7bca6d164
xolbynz <rnjs5162@gmail.com>
2023-05-24 10:04:01 +0900

    aws version downgrade

--- aws_cli_build.sh
+++ aws_cli_build.sh
@@ -2,10 +2,11 @@
 
 sudo apt install curl -y
 
-curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" -o "awscliv2.zip" && \
+curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64-2.0.30.zip" -o "awscliv2.zip" && \
 if [ -e "./awscliv2.zip" ]; then
     unzip awscliv2.zip && \
     sudo ./aws/install
 
     sudo rm -rf awscliv2.zip aws
 fi
+
